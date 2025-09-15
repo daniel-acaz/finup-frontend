4676b22fc99ffac697e328e01253ca79e3ab50b0
daniel-acaz <daniel_acaz@hotmail.com>
2025-09-15 15:33:19 -0300

    step 1: created a personalized scanner

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,8 +1,25 @@
-import { Html5QrcodeScanner } from 'html5-qrcode';
-import { useEffect } from 'react';
+import { Html5Qrcode, Html5QrcodeScannerState } from 'html5-qrcode';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './welcome.css';
+import { useFetchCameras } from '~/shared/useFetchCameras';
 
-const qrcodeRegionId = "html5qr-code-full-region";
+interface IInfoProps {
+  title: string
+}
+
+export const Info: React.FC<IInfoProps> = ({title}: IInfoProps) => (
+  <div>
+    <h6>{title}</h6>
+  </div>
+)
+
+enum PluginState {
+  Initial = 'initial',
+  Starting = 'starting',
+  Started = 'started',
+  StartingFailed = 'startingFailed',
+  StoppingFailed = 'stoppingFailed'
+}
 
 const itQrCodeSuccessCallback = (decodedText: string, decodedResult: any) => {
   console.log(`Code matched = ${decodedText}`, decodedResult);
@@ -14,20 +31,94 @@ const itQrCodeErrorCallback = (errorMessage: string) => {
 
 const config = { fps: 10, qrbox: { width: 250, height: 250 }, disableFlip: false, qrCodeSuccessCallback: itQrCodeSuccessCallback, itQrCodeErrorCallback };
 
-export function Welcome() {
+const qrcodeRegionId = "html5qr-code-full-region";
+
+export const Welcome = () => {
+
+  const {fetchCameras, state: {loading, error, cameraDevices}} = useFetchCameras();
+  const [selectedCameraId] = useState<string | undefined>(undefined);
+  const html5Qrcode = useRef<null | Html5Qrcode>(null);
+  const pluginStateRef = useRef<PluginState>(PluginState.Initial);
+  const qrCodeRegionRef = useRef<HTMLDivElement | null>(null);
+  const [isScannerReady, setIsScannerReady] = useState(false);
+  let cameraId: string | undefined
 
   useEffect(() => {
-    const verbose = false;
-
-    const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
-        html5QrcodeScanner.render(config.qrCodeSuccessCallback, config.itQrCodeErrorCallback);
-
-    return () => {
-            html5QrcodeScanner.clear().catch(error => {
-                console.error("Failed to clear html5QrcodeScanner. ", error);
-            });
-        };
+    fetchCameras();
   }, []);
+
+  useEffect(() => {
+    if (!isScannerReady) return;
+    html5Qrcode.current ??= new Html5Qrcode(qrcodeRegionId);
+    const prevQrcodeRegionId = qrcodeRegionId;
+    return () => {
+      if (html5Qrcode.current && prevQrcodeRegionId !== qrcodeRegionId) {
+        // stopping due changed qrcodeRegionId
+        html5Qrcode.current?.stop()
+          .then(() => {
+            console.log("Camera stopped due changed qrcodeRegionId");
+            // camera stopped
+          })
+          .catch(() => {
+            console.log("Camera failed to stop due changed qrcodeRegionId");
+            // camera failed to stop
+          });
+      }
+    };
+  }, [isScannerReady, qrcodeRegionId]);
+
+  useEffect(() => {
+    cameraId = selectedCameraId || cameraDevices[0]?.id;
+    if (!isScannerReady) return;
+    
+    if (
+      html5Qrcode.current &&
+      pluginStateRef.current !== PluginState.Starting &&
+      cameraId !== undefined
+    ) {
+      pluginStateRef.current = PluginState.Starting;
+      html5Qrcode.current
+        .start(cameraId, config, config.qrCodeSuccessCallback, config.itQrCodeErrorCallback)
+        .then(() => {
+          console.log("Camera started", qrCodeRegionRef.current);
+          pluginStateRef.current = PluginState.Started;
+        })
+        .catch(() => {
+          console.log("Camera failed to start");
+          pluginStateRef.current = PluginState.StartingFailed;
+        });
+    }
+    return () => {
+      if (html5Qrcode.current && pluginStateRef.current !== PluginState.Starting) {
+        html5Qrcode.current
+          ?.stop()
+          .then(() => {
+            console.log("Camera stopped");
+            pluginStateRef.current = PluginState.Initial;
+          })
+          .catch(() => {
+            console.log("Camera failed to stop");
+            pluginStateRef.current = PluginState.StoppingFailed;
+          });
+      }
+    };
+  }, [isScannerReady, config.qrCodeSuccessCallback]);
+
+
+  useEffect(() => {
+    if( cameraDevices.length === 0 ) return;
+    setIsScannerReady(true);
+  }, [cameraDevices]);
+
+  if (loading) {
+    return <Info title="Detecting available cameras"/>
+  }
+  if (error) {
+    return <Info title="Failed to detect cameras"/>
+  }
+  if (cameraDevices.length === 0) {
+    return <Info title="No available cameras"/>
+  }
 
   return (
     <main className='qr-code-scanner-content'>
@@ -36,7 +127,7 @@ export function Welcome() {
           Scan QR Code
         </h1>
         <p className='header'> Scan the invoice QR code to add its</p>
-        <div className='qr-code-scanner' id={qrcodeRegionId} style={{ width: "300px" }}></div>
+        <div className='qr-code-scanner' id={qrcodeRegionId} ref={qrCodeRegionRef}></div>
       </div>
     </main>
   );
